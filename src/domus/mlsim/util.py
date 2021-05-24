7abd607148673636f60616c5160eb46e7576a600
James Brusey <james.brusey@gmail.com>
2021-05-24 14:38:01 +0100

    add util

---
+++ src/domus/mlsim/util.py
@@ -0,0 +1,26 @@
+"""
+util.py
+
+Author
+------
+J. Brusey
+
+Date
+----
+May 24, 2021
+
+Description
+-----------
+
+various utility functions
+
+"""
+import numpy as np
+
+
+def kw_to_array(columns, **kwargs):
+    """convert a set of keywords into a numpy array according to a list
+    of column names"""
+
+    assert set(columns) == set(kwargs), f'Either additional {set(kwargs) - set(columns)} or missing {set(columns) - set(kwargs)} arguments'
+    return np.array([kwargs[c] for c in columns])
