aee8d79fe8378cae37e77fba670881480b298fb0
James Brusey <james.brusey@gmail.com>
2021-08-17 17:10:30 +0100

    fix missing scenario data

--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -5,5 +5,7 @@ def test_load_scenarios():
     scenarios = load_scenarios()
 
     assert scenarios.loc[1].ambient_t == 272.15
+    assert scenarios.loc[1].ambient_rh == 0.99
+    assert scenarios.loc[1].solar2 == 0
 
     assert scenarios.loc[28].pre_clo == 0.38
