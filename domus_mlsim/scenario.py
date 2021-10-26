16bd27a527e8c0a701d6c0b1a3ac220c45e529cb
James Brusey <james.brusey@gmail.com>
2021-10-26 12:09:16 +0100

    singleton for scenario

--- domus_mlsim/scenario.py
+++ domus_mlsim/scenario.py
@@ -10,13 +10,17 @@ J. Brusey, 24 May 2021
 
 Defines a scenario or start state for simulation.
 
-
+Revised to use a singleton approach
 
 """
 
 import pandas as pd
 import pkg_resources
 
+scenarios = pd.read_csv(
+    pkg_resources.resource_filename(__name__, "model/scenarios.csv")
+)
+
 
 def load_scenarios():
-    return pd.read_csv(pkg_resources.resource_filename(__name__, "model/scenarios.csv"))
+    return scenarios
